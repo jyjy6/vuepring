3b28ddc509e14816e8f640770a169a788bfeff25
jy <jungjuneyoung6@gmail.com>
2024-08-27 23:56:28 +0900

    0827loginLogic Changed

--- src/main/java/com/spvue/WebConfig.java
+++ src/main/java/com/spvue/WebConfig.java
@@ -3,6 +3,9 @@ package com.spvue;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -15,4 +18,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true); // 쿠키와 인증 헤더 포함
     }
+
+        @Bean
+        public HttpSessionEventPublisher httpSessionEventPublisher() {
+            return new HttpSessionEventPublisher();
+        }
+
+
 }
