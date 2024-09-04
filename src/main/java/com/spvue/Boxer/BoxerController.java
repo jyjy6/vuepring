4f67273e1269b2cec019880c82857df1f8af04e1
jy <jungjuneyoung6@gmail.com>
2024-09-04 23:57:03 +0900

    Function Added 0904 Boxer: Derived Query Method return Boxer Data

--- src/main/java/com/spvue/Boxer/BoxerController.java
+++ src/main/java/com/spvue/Boxer/BoxerController.java
@@ -3,10 +3,10 @@ package com.spvue.Boxer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.*;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -26,7 +26,12 @@ public class BoxerController {
     }
 
 //   겟매핑으로 복서의 체급을 받아서 해당 복서의 체급에 맞는 행들 다 가져와서 리스트에 넣어주세요~ 대신 정렬은 순위에 맞게 오름차순으로 해주세요.
-
+@GetMapping
+public List<Boxer> getAllBoxerByWeightClass(String weightClass) {
+    var result = boxerService.getAllBoxerByDivision(weightClass);
+    System.out.println(result);
+    return result;
+}
 
 
 }
--- src/main/java/com/spvue/Boxer/BoxerRepository.java
+++ src/main/java/com/spvue/Boxer/BoxerRepository.java
@@ -2,5 +2,11 @@ package com.spvue.Boxer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoxerRepository extends JpaRepository<Boxer, Long> {
+
+    List<Boxer> getAllBoxerByDivision(String weightClass);
+
+
 }
