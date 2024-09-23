320793798fa559c1b7f3c21bddcbba48d45473d2
jyjy <jungjuneyoung6@gmail.com>
2024-09-23 17:58:29 +0900

    0923

--- src/main/java/com/spvue/HomeVideo/HomeVideo.java
+++ src/main/java/com/spvue/HomeVideo/HomeVideo.java
@@ -7,6 +7,7 @@ import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Setter
@@ -17,10 +18,13 @@ public class HomeVideo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
-
+    @Lob // 큰 텍스트 데이터 저장
+    @Column(name = "content") // Optional: 열 이름 지정
     private String content;
     private String link;
     private String author;
+    @ElementCollection
+    private List<String> fileURLs; // 여러 파일 URL을 리스트로 받음
 
     @CreationTimestamp
     @Column(updatable = false)
