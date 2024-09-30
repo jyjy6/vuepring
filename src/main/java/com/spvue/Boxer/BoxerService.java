400f09e72c6c0ab48ad27de8539801f2cd2ff66e
jyjy <jungjuneyoung6@gmail.com>
2024-09-30 18:01:10 +0900

    1234

--- src/main/java/com/spvue/Boxer/BoxerService.java
+++ src/main/java/com/spvue/Boxer/BoxerService.java
@@ -18,7 +18,7 @@ public class BoxerService {
             System.out.println("Saving boxer: " + boxer);
             boxerRepository.save(boxer);
         } catch (Exception e) {
-            e.printStackTrace(); // 스택 트레이스 출력
+            e.printStackTrace();
         }
     }
 
@@ -43,22 +43,19 @@ public class BoxerService {
             // 새로운 랭킹이 더 높은 경우 (예: 5위에서 2위로 올라감)
             List<Boxer> affectedBoxers =
                     boxerRepository.findByDivisionAndRankingBetween( boxer.getDivision(), newRanking, oldRanking - 1);
-            System.out.println("해당복서 :" + affectedBoxers);
             for (Boxer box : affectedBoxers) {
-                box.setRanking(box.getRanking() + 1); // 랭킹을 한 단계씩 내림
+                box.setRanking(box.getRanking() + 1); // 각 복서의 랭킹을 한 단계씩 내림
                 boxerRepository.save(box);
             }
         } else if (newRanking > oldRanking) {
             // 새로운 랭킹이 더 낮은 경우 (예: 2위에서 5위로 내려감)
 
             List<Boxer> affectedBoxers = boxerRepository.findByDivisionAndRankingBetween(boxer.getDivision(),oldRanking + 1, newRanking);
-            System.out.println("해당복서 :" + affectedBoxers);
             for (Boxer box : affectedBoxers) {
-                box.setRanking(box.getRanking() - 1); // 랭킹을 한 단계씩 올림
+                box.setRanking(box.getRanking() - 1); // 각 복서의 랭킹을 한 단계씩 올림
                 boxerRepository.save(box);
             }
         }
-
 
     }
 
